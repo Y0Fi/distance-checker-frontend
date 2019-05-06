981e8211249a71aee004c448dca47d0b3afcaca4
Y0Fi <terlecki.andras@gmail.com>
2019-05-06 20:36:21 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section v-loading="loading">
+        <el-table :data="distanceData" style="width: 100%">
+            <el-table-column prop="name" label="Name"/>
+            <el-table-column prop="distance" label="Distance"/>
+        </el-table>
+
+        <section>
+            <el-row>
+                <el-col :span="6"><el-input placeholder="Enter first word" v-model="words.word1"></el-input></el-col>
+                <el-col :span="6"><el-input placeholder="Enter second word" v-model="words.word2"></el-input></el-col>
+                <el-col :span="6"><el-input placeholder="Enter third word" v-model="words.word3"></el-input></el-col>
+                <el-col :span="6"><el-button type="success" plain v-on:click="compareStrings">Compare</el-button></el-col>
+            </el-row>
+            <el-alert :title="anagramCheckResult" type="success" v-if="anagramCheckResult"/>
+            <el-alert :title="anagramCheckError" type="error" v-if="anagramCheckError"/>
+        </section>
+
+
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data: () => ({
+            distanceData: [],
+            words: {
+                word1: '',
+                word2: '',
+                word3: '',
+            },
+            anagramCheckResult: '',
+            anagramCheckError: '',
+            loading: false,
+        }),
+        created() {
+            this.loading = true;
+            axios.get('http://localhost:3000/')
+                .then(response => {
+                    this.loading = false;
+                    this.distanceData = response.data;
+                })
+                .catch(() => {
+                    this.loading = false;
+                })
+        },
+        methods: {
+            compareStrings() {
+                this.anagramCheckResult = '';
+                this.anagramCheckError = '';
+                this.loading = true;
+                axios.post('http://localhost:3000/', {words: Object.values(this.words)})
+                    .then(response => {
+                        this.loading = false;
+                        this.anagramCheckResult = response.data;
+                    })
+                    .catch(error => {
+                        this.loading = false;
+                        this.anagramCheckError = error.response.data;
+                    })
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
